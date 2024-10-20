c446eca4404a6db86395b7dc27eaf2764da8d439
Heverton Luan Roieski <hevertonlr@gmail.com>
2024-10-19 21:20:43 -0300

    feat: add page loading, bootstrap and change to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
-import { MenuLateralComponent } from "./shared/components/menu-lateral/menu-lateral.component";
+import { MenuLateralComponent } from './shared/components/menu-lateral/menu-lateral.component';
 import { PaginaLoginService } from './shared/services/pagina-login.service';
 import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+import { LoadingBarService } from '@ngx-loading-bar/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MenuLateralComponent, ToolbarComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MenuLateralComponent,
+    ToolbarComponent,
+    LoadingBarRouterModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'labPCP';
-  constructor(public router: Router, public paginaLoginService: PaginaLoginService) {}
+  constructor(
+    public router: Router,
+    public paginaLoginService: PaginaLoginService,
+    public readonly loader: LoadingBarService
+  ) {}
 
   get isLoggedIn(): boolean {
     return this.paginaLoginService.isLoggedIn();
   }
-
- 
 }
